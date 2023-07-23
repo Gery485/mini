4a9786b64f4476f8fd6925538d2e7d5268a57914
Gery485 <n.gerdom@gmail.com>
2023-07-23 16:22:45 +0200

    öö

--- my_robot_controller/scripts/moveTheRobotWithNodes.py
+++ my_robot_controller/scripts/moveTheRobotWithNodes.py
@@ -83,31 +83,31 @@ def main():
              # Movement control based on distance
             if distance_cm > 300:
                 cmd_vel_msg.linear.x = point2
-                cmd_vel_msg.angular.z = zero
+                cmd_vel_msg.angular.z = 3
                 cmd_vel_pub.publish(cmd_vel_msg)
                 rospy.sleep(sleep)
             elif distance_cm <= 300:
                 cmd_vel_msg.linear.x = point2
-                cmd_vel_msg.angular.z = zero
+                cmd_vel_msg.angular.z = 3
                 cmd_vel_pub.publish(cmd_vel_msg)
                 rospy.sleep(sleep)
             elif distance_cm <= 100:
-                cmd_vel_msg.linear.x = point2/2
-                cmd_vel_msg.angular.z = zero
+                cmd_vel_msg.linear.x = point2
+                cmd_vel_msg.angular.z = -6
                 cmd_vel_pub.publish(cmd_vel_msg)
                 rospy.sleep(sleep)
-
-            elif distance_cm < minDistance:
-            # Move with minPoint2 linear velocity and point2 angular velocity
-             cmd_vel_msg.linear.x = minPoint2
-             cmd_vel_msg.angular.z = point2
-             cmd_vel_pub.publish(cmd_vel_msg)
-
-            else:
-            # Move forward with point2 linear velocity and point2 angular velocity
-             cmd_vel_msg.linear.x = point2
-             cmd_vel_msg.angular.z = point2
-             cmd_vel_pub.publish(cmd_vel_msg)
+                cmd_vel_msg.linear.x = point2
+                cmd_vel_msg.angular.z = 6
+                cmd_vel_pub.publish(cmd_vel_msg)
+                rospy.sleep(sleep/2)
+                cmd_vel_msg.linear.x = point2
+                cmd_vel_msg.angular.z = -3
+                cmd_vel_pub.publish(cmd_vel_msg)
+                rospy.sleep(sleep)
+            elif distance_cm <= minDistance:
+                 cmd_vel_msg.linear.x = minPoint2
+                 cmd_vel_msg.angular.z = point2
+                 cmd_vel_pub.publish(cmd_vel_msg)
 
         else:
             print("Unable to calculate the distance to the obstacle.")

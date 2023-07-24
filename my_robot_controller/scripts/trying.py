c83633932761ecfcff03cb8292a82d5e5773d3e6
Gery485 <n.gerdom@gmail.com>
2023-07-24 16:20:40 +0200

    kdsjflj

--- my_robot_controller/scripts/trying.py
+++ my_robot_controller/scripts/trying.py
@@ -8,8 +8,10 @@ if distance_cm < 135:
 
 #snake:
             if distance_cm > 300:
-                cmd_vel_msg.linear.x = point2
-                cmd_vel_msg.angular.z = 3
+                start1 = time.time()
+                if time.time() - start1 < 1:
+                  cmd_vel_msg.linear.x = point2
+                 cmd_vel_msg.angular.z = 3
                 cmd_vel_pub.publish(cmd_vel_msg)
                 rospy.sleep(sleep)
             elif distance_cm <= 300:
@@ -66,3 +68,56 @@ if distance_cm < 135:
              cmd_vel_msg.linear.x = point2
              cmd_vel_msg.angular.z = point2
              cmd_vel_pub.publish(cmd_vel_msg)
+
+
+
+
+              start = time.time()
+                start1 = time.time()
+                while time.time() - start < 1.1:
+                    cmd_vel_msg.linear.x = zero
+                    cmd_vel_msg.angular.z = zero
+                    cmd_vel_pub.publish(cmd_vel_msg)
+                    rate.sleep()
+
+                while time.time() - start1 < 1.1:
+                    cmd_vel_msg.linear.x = zero
+                    cmd_vel_msg.angular.z = turn
+                    cmd_vel_pub.publish(cmd_vel_msg)
+                    rate.sleep()
+                continue
+
+            else:
+                start1 = time.time()
+                start2 = time.time()
+                start3 = time.time()
+                start4 = time.time()
+                start5 = time.time()
+                
+
+                while time.time() - start1 < 1.1:
+                  cmd_vel_msg.linear.x = move
+                  cmd_vel_msg.angular.z = zero
+                  cmd_vel_pub.publish(cmd_vel_msg)
+                  rate.sleep()
+                while time.time() - start2 < 1.1:
+                  cmd_vel_msg.linear.x = zero
+                  cmd_vel_msg.angular.z = turn
+                  cmd_vel_pub.publish(cmd_vel_msg)
+                  rate.sleep()
+                while time.time() - start3 < 1.1:
+                  cmd_vel_msg.linear.x = zero
+                  cmd_vel_msg.angular.z = turn * -1
+                  cmd_vel_pub.publish(cmd_vel_msg)
+                  rate.sleep()
+                while time.time() - start4 < 1.1:
+                  cmd_vel_msg.linear.x = zero
+                  cmd_vel_msg.angular.z = turn * -1
+                  cmd_vel_pub.publish(cmd_vel_msg)
+                  rate.sleep()
+                while time.time() - start5 < 1.1:
+                  cmd_vel_msg.linear.x = zero
+                  cmd_vel_msg.angular.z = turn
+                  cmd_vel_pub.publish(cmd_vel_msg)
+                  rate.sleep()
+                continue
